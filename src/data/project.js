15a5610cd77736b01a697a9177efc6785185fee6
Naag Jayanthy <njayanthy@ad.mtrust.com>
2019-05-06 12:22:04 -0500

    app

---
+++ src/data/project.js
@@ -0,0 +1,28 @@
+import project1 from '../assets/images/project1.png';
+import project2 from '../assets/images/project2.png';
+import project3 from '../assets/images/project3.png';
+const PROJECTS =[
+    {
+        id:1,
+        title : 'Example React application 1 ',
+        description : 'A react app  built ,involving js and core web dev concepts',
+        link : "www.google.com",
+        image : project1
+    },
+    {
+        id:2,
+        title : 'Example React application 2 ',
+        description : 'A react app I built ,involving js and core web dev concepts',
+        link : "www.google.com",
+        image : project2
+    }
+    ,{
+        id:3,
+        title : 'Example React application 3',
+        description : 'A react app I built ,involving js and core web dev concepts',
+        link : "www.google.com",
+        image : project3
+    }
+
+];
+export default PROJECTS;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/images/email_icon.png';
+import githubIcon from '../assets/images/github_icon.png';
+import twitterIcon from '../assets/images/twitter_icon.png';
+import linkedinIcon from'../assets/images/linkedin_icon.png';
+
+const SOCIAL_PROFILES = [
+{
+    id:1,
+    link : 'mailto: jayanthymohit6@gmail.com',
+    image: emailIcon 
+},
+{
+    id:2,
+    link : 'https://github.com',
+    image: githubIcon 
+},
+{
+    id:3,
+    link : 'https://www.linkedin.com',
+    image: linkedinIcon 
+},
+{
+    id:4,
+    link : 'https://twitter.com',
+    image: twitterIcon 
+}
+
+];
+export default SOCIAL_PROFILES;
